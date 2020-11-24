6ba199d9c21154a4f659300c7bba540955867855
Sebastien Hoarau <shu@suneris.com>
2020-11-24 09:46:58 +0100

    refactor

--- app.py
+++ app.py
@@ -3,50 +3,45 @@ from flask_sqlalchemy import SQLAlchemy
 
 DATABASE = "./todo.db"
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
 
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    complete = db.Column(db.Boolean)
-
-
-@app.route("/")
-def index():
-    todo_list = Todo.query.all()
-    return render_template("index.html", todo_list=todo_list)
-
-
-@app.route("/add", methods=["POST"])
-def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-@app.route("/complete/<string:todo_id>")
-def complete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-@app.route("/delete/<string:todo_id>")
-def delete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-if __name__ == "__main__":
+def create_app() -> Flask:
+    app = Flask(__name__)
     # uncomment below to use a file rather than memory database
     # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DATABASE
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    db = SQLAlchemy(app)
+
+    class Todo(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(80))
+        complete = db.Column(db.Boolean)
+
+    @app.route("/")
+    def index():
+        todo_list = Todo.query.all()
+        return render_template("index.html", todo_list=todo_list)
+
+    @app.route("/add", methods=["POST"])
+    def add():
+        title = request.form.get("title")
+        new_todo = Todo(title=title, complete=False)
+        db.session.add(new_todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+
+    @app.route("/complete/<string:todo_id>")
+    def complete(todo_id):
+        todo = Todo.query.filter_by(id=todo_id).first()
+        todo.complete = not todo.complete
+        db.session.commit()
+        return redirect(url_for("index"))
+
+    @app.route("/delete/<string:todo_id>")
+    def delete(todo_id):
+        todo = Todo.query.filter_by(id=todo_id).first()
+        db.session.delete(todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+
     db.create_all()
-    app.run(debug=True)
+    return app
